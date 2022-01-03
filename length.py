f71444d2630feff8b088f2028f7b40b96c50a139
thanhhafmcvn <83865348+MLucastero@users.noreply.github.com>
2022-01-03 21:31:24 +0700

    Add files via upload

---
+++ length.py
@@ -0,0 +1,16 @@
+
+while True:
+        print('')
+        print('')
+        print("===================================") 
+        print("Note: Ctrl C to quit the programme!")
+        print("-----------------------------------") 
+        a = (input("Type something: "))
+        print("-----------------------------------")
+        print("Length of your text is: " + str(len(a))) 
+        print("===================================")
+        print('')
+        print('')
+        print('')
+        print('')
+   
